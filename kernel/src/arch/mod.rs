3c1e7d2e3fd86ed7da7d793c7994e45ed03c959c
Sean Cross <sean@xobs.io>
2021-05-27 22:12:19 +0800

    remove riscv
    Signed-off-by: Sean Cross <sean@xobs.io>

--- kernel/src/arch/mod.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2020 Sean Cross <sean@xobs.io>
-// SPDX-License-Identifier: Apache-2.0
-
-#[cfg(target_arch = "riscv32")]
-mod riscv;
-#[cfg(target_arch = "riscv32")]
-pub use crate::arch::riscv::*;
